fc7bc1f8e561ea1ad51d27f05a9e395f841a841f
spartakbelovfloresku@gmail.com <spartakbelovfloresku@gmail.com>
2023-05-14 06:34:56 -0500

    created db with catigories and use it for frontend

--- shop/src/utils/firebase/firebase.utils.js
+++ shop/src/utils/firebase/firebase.utils.js
@@ -14,6 +14,10 @@ import {
     doc,
     getDoc,
     setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs,
 } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -48,6 +52,31 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 export const singInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach(obj => {
+        const docRef = doc(collectionRef, obj.title.toLowerCase())
+        batch.set(docRef, obj);
+    });
+    await batch.commit();
+}
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const {title, items} = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    },{});
+
+    return categoryMap;
+}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalFields = {}) => {
     if (!userAuth) return;
