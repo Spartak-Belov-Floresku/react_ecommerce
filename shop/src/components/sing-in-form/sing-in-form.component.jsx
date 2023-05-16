22035b146b7170083aff1530aedad5fbc90c8527
spartakbelovfloresku@gmail.com <spartakbelovfloresku@gmail.com>
2023-05-16 03:30:02 -0500

    converted style to componet style button

--- shop/src/components/sing-in-form/sing-in-form.component.jsx
+++ shop/src/components/sing-in-form/sing-in-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import { FormInput } from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import {
     signInWithGooglePopup,
@@ -73,7 +73,7 @@ const SingInForm = () => {
                 />
                 <div className="buttons-container">
                     <Button type="submit">Sing In</Button>
-                    <Button type="button" buttonType="google" onClick={singInWithGoogle}>Google Sing In</Button>
+                    <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={singInWithGoogle}>Google Sing In</Button>
                 </div>
             </form>
         </div>
