5c991772363605c7782222c0960d9a97c30bd1c7
spartakbelovfloresku@gmail.com <spartakbelovfloresku@gmail.com>
2023-05-22 16:29:07 -0500

    added redusers to user and categories

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,15 +1,17 @@
-import { useContext, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { selectCategoriesMap } from "../../store/categories/category.selector";
+import ProductCard from '../../components/product-card/product-card.commponent';
 
 import './category.styles.scss';
-import { CategoriesContext } from '../../contexts/categories.context';
-import ProductCard from '../../components/product-card/product-card.commponent';
 
 
 const Catgory = () => {
     const { category } = useParams();
-    const { categoriesMap } = useContext(CategoriesContext);
-    const [ products, setProducts ] = useState([]);
+    const categoriesMap = useSelector(selectCategoriesMap);
+    const [ products, setProducts ] = useState(categoriesMap[category]);
 
     useEffect(() => {
         setProducts(categoriesMap[category]);
