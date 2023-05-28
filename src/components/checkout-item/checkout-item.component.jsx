3f5db1d17e0caf1c7f93c163dcd0686a8deed98b
spartakbelovfloresku@gmail.com <spartakbelovfloresku@gmail.com>
2023-05-28 07:46:52 -0500

    removed cart context

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,11 @@
-import { useContext } from 'react';
-import { CartContext } from '../../contexts/cart.context';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectCartItems } from '../../store/cart/cart.selector';
+import {
+    addItemToCart,
+    clearItemFromCart,
+    removeItemFromCart
+} from '../../store/cart/cart.action';
 
 import {
     CheckoutItemContainer,
@@ -14,13 +20,16 @@ import {
 
 const CheckoutItem = ({cartItem}) => {
 
-    const { addItemToCart, removeItemFromCart, clearItemFromCart } = useContext(CartContext);
-
-    const clearItemHandler = () => clearItemFromCart(cartItem);
-    const addItemHandler = () => addItemToCart(cartItem);
-    const removeItemHndler = () => removeItemFromCart(cartItem);
+    const dispatch =  useDispatch();
+    const cartItems = useSelector(selectCartItems);
 
     const {name, imageUrl, price, quantity} = cartItem;
+
+    const clearItemHandler = () => dispatch(clearItemFromCart(cartItems, cartItem));
+    const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+    const removeItemHndler = () => dispatch(removeItemFromCart(cartItems, cartItem));
+
+
 
     return (
         <CheckoutItemContainer>
