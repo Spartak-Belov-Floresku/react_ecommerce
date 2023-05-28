a83e665b80bcc07d2cb8da4a1dc6b85c117018a5
spartakbelovfloresku@gmail.com <spartakbelovfloresku@gmail.com>
2023-05-28 05:09:42 -0500

    applied createSelector to the user

--- src/store/user/user.selector.js
+++ src/store/user/user.selector.js
@@ -1,1 +1,15 @@
-export const selectCurrentUser = state => state.user.currentUser;
+import { createSelector } from "reselect";
+
+const selectUserReducer = state => state.user;
+
+const selectUser = createSelector(
+    [selectUserReducer],
+    userSlice => userSlice.currentUser
+);
+
+// export const selectCurrentUser = state => state.user.currentUser;
+
+export const selectCurrentUser = createSelector(
+    [selectUser],
+    currentUser => currentUser
+)
