abc6ef2c26faef6e71364bc76fa07966cf5e972f
spartakbf <spartak.belovfloresku@firsthorizon.com>
2024-10-31 17:05:51 -0400

    added redux PersistGate

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 // import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
@@ -20,8 +22,18 @@ const loggerMiddleware = store => next => action => {
     console.log('next state: ', store.getState());
 }
 
-const middleWares = [loggerMiddleware];
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [process.env.NODE_ENV === 'production' && loggerMiddleware].filter(Boolean);
 
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composeEnhancers);
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persistor = persistStore(store);
